488f49f8b1b9f97a5039e506580b5f4d5833d27b
Anderson Jorge <anderson.jorge33@gmail.com>
2021-02-27 20:08:37 -0300

    arquivo da aula sobre função map() basico

---
+++ array/map1.js
@@ -0,0 +1,19 @@
+const nums = [1, 2, 3, 4, 5]
+
+// for com propósito
+let resultado = nums.map(function(elemento) {
+    return elemento * 2
+})
+
+console.log(resultado)
+console.log(nums)
+
+const soma10 = elemento => elemento + 10
+const triplo = elemento => elemento * 3
+const paraDinheiro = elemento => `R$ ${parseFloat(elemento).toFixed(2).replace('.', ',')}`
+
+resultado = nums.map(soma10).map(triplo).map(paraDinheiro)
+console.log(resultado)
+
+
+
