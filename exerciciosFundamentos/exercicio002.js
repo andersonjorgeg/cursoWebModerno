98939511874bb714418022ddb88117aff8bf7631
Anderson Jorge <anderson.jorge33@gmail.com>
2021-03-02 13:12:04 -0300

    arquivo do exercicio 002 fundamentos js

---
+++ exerciciosFundamentos/exercicio002.js
@@ -0,0 +1,14 @@
+/* Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+
+Obs: considere que um ano tem 365 dias. Desconsidere anos bissextos (com 366 dias) e
+desconsidere também dias decorridos desde o último aniversário.
+
+ */
+
+function converterIdadeEmAnosParaDias(idade) {
+    const anos = idade * 365
+    return anos
+}
+
+console.log(converterIdadeEmAnosParaDias(25))
+console.log(converterIdadeEmAnosParaDias(70))
