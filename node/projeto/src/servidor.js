9eed60eeb7e7002aa10b98f273663e31327fcecf
Anderson Jorge <anderson.jorge33@gmail.com>
2021-03-10 20:03:12 -0300

    Projeto: API com Express #2

--- node/projeto/src/servidor.js
+++ node/projeto/src/servidor.js
@@ -2,9 +2,22 @@ const porta = 3003
 
 const express = require('express')
 const app = express()
+const bancoDeDados = require('./bancoDeDados')
 
 app.get('/produtos', (req, res, next) => {
-    res.send({ nome: 'Notebook', preco: 123.45}) //? converter para JSON
+    res.send(bancoDeDados.getProdutos())
+})
+
+app.get('/produtos/:id', (req, res, next) => {
+    res.send(bancoDeDados.getProduto(req.params.id))
+})
+
+app.post('/produtos', (req, res, next) => {
+    const produto = bancoDeDados.salvarProduto({
+        nome: req.body.name,
+        preco: req.body.preco
+    })
+    res.send(produto) //? vai gerar um JSON
 })
 
 app.listen(porta, () => {
