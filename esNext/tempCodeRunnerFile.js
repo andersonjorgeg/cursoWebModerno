5016f282f3a17e648810b323e08a3100c8a4a8be
Anderson Jorge <anderson.jorge33@gmail.com>
2021-03-16 20:06:17 -0300

    arquivo da aula sobre Promise #2

--- esNext/tempCodeRunnerFile.js
+++ esNext/tempCodeRunnerFile.js
@@ -1,1 +1,9 @@
-console.log(typeof Promise)
+let p3 = new Promise(function (resolve) {
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel'])
+})
+
+p3.then(function(valor) {
+    for (let nome of valor) {
+        console.log(nome)
+    }
+})
