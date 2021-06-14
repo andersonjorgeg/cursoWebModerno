503ffec66898b99ab200efdbf8052234e026cde7
Anderson Jorge <anderson.jorge33@gmail.com>
2021-06-14 16:02:21 -0300

    Selecionando Elementos da DOM #2

---
+++ dom/js/tranfomarArray.js
@@ -0,0 +1,19 @@
+/* transformando em um array */
+
+const lista1 = [1,2,3,4,5]
+const lista2 = [6,7,8,9]
+
+const listaJunta = [...lista1, ...lista2]
+
+
+console.log(listaJunta[0])
+console.log(listaJunta[1])
+console.log(listaJunta[2])
+console.log(listaJunta[3])
+console.log(listaJunta[4])
+console.log(listaJunta[5])
+console.log(listaJunta[6])
+console.log(listaJunta[7])
+console.log(listaJunta[8])
+console.log(listaJunta[9])
+console.log(listaJunta[10])
