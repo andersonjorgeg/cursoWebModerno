74f86831e97116326b2c80e6165cb275172ee404
Anderson Jorge <anderson.jorge33@gmail.com>
2021-06-28 13:04:53 -0300

    arquivo do modulo webpack - Configurando a Pasta de Saída

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -2,5 +2,9 @@ const webpack = require('webpack')
 
 module.exports = {
     mode: 'development',
-    entry: './src/principal.js'
+    entry: './src/principal.js',
+    output: {
+        filename: 'principal.js',
+        path: __dirname + '/public'
+    }
 }
