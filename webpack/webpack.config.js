07a7f875531c0e5cb0eeccfbac289ed89f261379
Anderson Jorge <anderson.jorge33@gmail.com>
2021-06-28 18:31:18 -0300

    arquivo do modulo webpack - Desenvolvimento & Produção

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,17 +1,32 @@
+const modoDev = process.env.NODE_ENV !== 'production'
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimizer: [
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
         new MiniCssExtractPlugin ({
             filename: "estilo.css"
-        })
+        }),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
     ],
     module: {
         rules: [{
